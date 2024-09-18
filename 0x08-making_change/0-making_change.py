90557e53dc5729ac04e369af3d353ea897139dec
Stanley <stanleyemeke71@gmail.com>
2024-09-18 03:40:17 +0000

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
+"""
+This script defines a function to calculate the minimum number of coins
+required to achieve a given total.
+"""
 
 def makeChange(coins, total):
-    # If total is 0 or less, we need 0 coins
+    """
+    Determines the fewest number of coins needed to meet a given total.
+    
+    Arguments:
+    coins -- a list of integers representing coin denominations.
+    total -- the total amount we want to achieve using the coins.
+    
+    Returns:
+    The fewest number of coins needed to meet the total.
+    If total is 0 or less, returns 0.
+    If the total cannot be met by any number of coins, returns -1.
+    """
     if total <= 0:
         return 0
-    
-    # Initialize an array to store the minimum coins for each amount from 0 to total
-    # Set the values to a large number (infinity) initially
+
     dp = [float('inf')] * (total + 1)
-    
-    # Base case: 0 coins are needed to make the total 0
     dp[0] = 0
-    
-    # Iterate through all amounts from 1 to total
+
     for i in range(1, total + 1):
-        # For each coin, check if it can be used to reach the current amount
         for coin in coins:
             if i - coin >= 0:
                 dp[i] = min(dp[i], dp[i - coin] + 1)
-    
-    # If the value at dp[total] is still infinity, the total cannot be met
+
     return dp[total] if dp[total] != float('inf') else -1
 
